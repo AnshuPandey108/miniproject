1ecfd13679092a90bed306929fe0897c468ad7cb
Anshu Pandey <anshu.pandey@avista.com>
2025-08-31 16:01:06 +0530

    minor changes

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,16 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./src/config/db.js";
-
+import authRoutes from "./src/routes/auth.js";
+import projectRoutes from "./src/routes/projects.js";
+import taskRoutes from "./src/routes/tasks.js";
 dotenv.config();
 connectDB();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-import authRoutes from "./src/routes/auth.js";
-import projectRoutes from "./src/routes/projects.js";
-import taskRoutes from "./src/routes/tasks.js";
+
 app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/tasks", taskRoutes);
